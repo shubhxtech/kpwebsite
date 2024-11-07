1efc4366be52407994afd7d9c95309ca66f60f87
nitin517 <nitinchaurasiya1749@gmail.com>
2024-11-07 19:09:02 +0530

    Updated App.jsx with Router Setup

--- kpwebsite/src/App.jsx
+++ kpwebsite/src/App.jsx
@@ -1,11 +1,25 @@
-import hero from '../src/components/hero'
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/header';
+import Footer from './components/footer';
+import Home from './pages/home';
+import Gsoc from './pages/gsoc';
+import Teams from './pages/teams';
 
-  return (
-    <>
-    <hero/>
-    </>
-  )
+function App() {
+    return (
+        <Router>
+            <Header /> {/* This will appear on every page */}
+            <div className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/gsoc" element={<Gsoc />} />
+                    <Route path="/teams" element={<Teams />} />
+                </Routes>
+            </div>
+            <Footer /> {/* This will also appear on every page */}
+        </Router>
+    );
 }
 
-export default App
+export default App;
