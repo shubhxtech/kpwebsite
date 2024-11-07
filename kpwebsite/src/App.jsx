29e590a982ab2286fc7a2b8895f7da60fac2bf67
Kaustubh <kaustubh0410@gmail.com>
2024-11-07 22:40:19 +0530

    Teams Page

--- kpwebsite/src/App.jsx
+++ kpwebsite/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
-import Footer from './components/footer';
+import Footer from './components/footer/Footer';
 import Home from './pages/home';
 import Gsoc from './pages/gsoc';
 import Teams from './pages/teams';
